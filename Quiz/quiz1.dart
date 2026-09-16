c5bd042808a292a26806f056429bcc7ce3a8a939
warmanabdurrachman5-ops <warmanabdurrachman5@gmaail.com>
2026-09-16 19:58:14 +0700

    Tulis pesan atau catatan perubahan di siniQuiz1

--- Quiz/quiz1.dart
+++ Quiz/quiz1.dart
@@ -1,10 +1,18 @@
-void tampilkanMenu() {
-  print('=== MENU KANTINKAMPUS ===');
-  print('Nasi Goreng');
-  print('Ayam Geprek');
-  print('Es Teh Manis');
-}
+void main(){
+  String namaProduk ="nasi goreng";
+  int harga = 15000;
+  int jumlahBeli = 3;
+  double pajakpersen = 0.10;
 
-void main() {
-  tampilkanMenu();
+  int subtotal = harga * jumlahBeli;
+  double nominalpajak = subtotal * pajakpersen;
+  double totalbayar = subtotal + nominalpajak;
+
+  print("Nama Produk : $namaProduk");
+  print("Harga       : $harga");
+  print("Jumlah Beli : $jumlahBeli");
+  print("Subtotal    : Rp.$subtotal");
+
+  print("Pajak 10%   :Rp.${nominalpajak.toInt()}");
+  print("Total Bayar :Rp.${totalbayar.toInt()}");
 }
