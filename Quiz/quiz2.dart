b6c736ef830dc52e0f2e5f89d399934977af1158
warmanabdurrachman5-ops <warmanabdurrachman5@gmaail.com>
2026-09-16 21:19:36 +0700

    sudah benar

--- Quiz/quiz2.dart
+++ Quiz/quiz2.dart
@@ -1,1 +1,16 @@
-1
+void main(){
+  String namaMahasiswa = "Andi";
+  int nilai = 78;
+
+  String status;
+
+  if (nilai >= 75){
+    status = "Lulus";
+  } else {
+    status ="Tidak Lulus";
+  }
+
+  print("Nilai Mahasiswa : $namaMahasiswa");
+  print("Nilai           : $nilai");
+  print("Status          : $status");
+}
